de01069d3f8e087f5c2f279973434ec478cf4ba1
Natrix <osken_o.s@hotmail.com>
2025-10-28 20:49:41 +0100

    At least it could technically work now

--- src/root.zig
+++ src/root.zig
@@ -1,23 +1,90 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
+const CompareOperator = std.math.CompareOperator;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+pub const Mapper = union(enum) {
+    field: []const u8,
+    getter: []const u8,
+    value,
 
-    try stdout.flush(); // Don't forget to flush!
+    pub fn cmp(comptime mapper: Mapper, comptime op: CompareOperator, lhs: anytype, rhs: anytype) bool {
+        const lhs_val, const rhs_val = switch (mapper) {
+            inline .field => |f| .{ @field(lhs, f), @field(rhs, f) },
+            inline .getter => |g| .{ @field(lhs, g)(lhs), @field(rhs, g)(rhs) },
+            inline .value => .{ lhs, rhs },
+        };
+        return std.math.compare(lhs_val, op, rhs_val);
+    }
+};
+
+/// Sort a slice on a Mapper (in descending order)
+pub fn sortDesc(comptime mapper: Mapper, comptime T: type, items: []T) []T {
+    std.mem.sort(T, items, {}, struct {
+        pub fn lessThan(_: void, lhs: T, rhs: T) bool {
+            return mapper.cmp(.gt, lhs, rhs);
+        }
+    }.lessThan);
+    return items;
 }
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
+/// Sort a slice on a Mapper (in ascending order)
+pub fn sortAsc(comptime mapper: Mapper, comptime T: type, items: []T) []T {
+    std.mem.sort(T, items, {}, struct {
+        pub fn lessThan(_: void, lhs: T, rhs: T) bool {
+            return mapper.cmp(.lt, lhs, rhs);
+        }
+    }.lessThan);
+    return items;
 }
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
+pub fn select(comptime mapper: Mapper, comptime T: type, comptime len: usize, items: anytype) [len]T {
+    var result: [len]T = undefined;
+    for (&result, items[0..len]) |*r, item| r.* = switch (mapper) {
+        inline .field => |f| @field(item, f),
+        inline .getter => |g| @field(item, g)(item),
+        inline .value => item,
+    };
+    return result;
+}
+
+/// Deduplicates on a Mapper by swap removing, the `items` must be sorted.
+pub fn dedupOn(comptime mapper: Mapper, T: type, items: []T) []T {
+    var result = items;
+    var idx: usize = 1;
+    while (idx < result.len) {
+        if (mapper.cmp(.eq, result[idx - 1], result[idx])) {
+            std.mem.swap(T, &result[idx], &result[result.len - 1]);
+            result = result[0 .. result.len - 1];
+        } else idx += 1;
+    }
+    return result;
+}
+
+pub fn copy(comptime T: type, comptime items: []const T) [items.len]T {
+    var result: [items.len]T = undefined;
+    @memcpy(&result, items);
+    return result;
+}
+
+test "ashedtio" {
+    try std.testing.expectEqual(65535, std.math.maxInt(u16));
+}
+
+test "If we can instantiate new arrays through dereferencing" {
+    const patterns: []const [:0]const u8 = &.{
+        "&amp;", "&gt;", "&lt;", "&quote;", "&apos;", "12345"
+    };
+
+    const P = @TypeOf(patterns[0]);
+    const by_length = comptime init: {
+        var res = copy(P, patterns);
+        _ = sortAsc(.{ .field = "len" }, P, &res);
+        break :init res;
+    };
+    const lengths = (comptime init: {
+        var vals = select(.{ .field = "len" }, usize, by_length.len, &by_length);
+        break :init copy(usize, sortAsc(.value, usize, dedupOn(.value, usize, &vals)));
+    });
+    try std.testing.expectEqualSlices(usize, &.{ 4, 5, 6, 7 }, &lengths);
 }
--- src/tokenize.zig
+++ src/tokenize.zig
@@ -1,73 +1,49 @@
 const std = @import("std");
 
-fn Alternative(comptime patterns: []const [:0]const u8) type {
-    const P = @TypeOf(patterns[0]);
-    comptime var sorted_patterns: [patterns.len]P = undefined;
-    @memcpy(&sorted_patterns, patterns);
-    std.mem.sort(P, &sorted_patterns, .{}, struct {
-        pub fn lessThan(_: anytype, lhs: P, rhs: P) bool {
-            const len = @min(lhs.len, rhs.len);
-            for (lhs[0..len], rhs[0..len]) |l, r| if (l != r)
-                return l < r;
-            return lhs.len < rhs.len;
-        }
-    }.lessThan);
-    const pats = sorted_patterns;
+const Allocator = std.mem.Allocator;
 
-    comptime var init_enum_fields: [pats.len]std.builtin.Type.EnumField = undefined;
-    for (&init_enum_fields, pats, 0..) |*ef, p, i|
+const root = @import("root.zig");
+
+fn MatchCursor(comptime patterns: []const [:0]const u8) type {
+    const P = @TypeOf(patterns[0]);
+    const Flags = @Type(.{ .int = .{ .bits = patterns.len, .signedness = .unsigned } });
+    const by_length = (comptime init: {
+        var res = root.copy(P, patterns);
+        _ = root.sortDesc(.{ .field = "len" }, P, &res);
+        break :init res;
+    });
+
+    const lengths = (comptime init: {
+        var data = root.select(.{ .field = "len" }, usize, by_length.len, by_length);
+        const deduped = root.dedupOn(.value, usize, &data);
+        _ = root.sortDesc(.value, usize, deduped);
+        var result: [deduped.len]struct { len: usize, flags: Flags } = undefined;
+        var r_idx: usize = 0;
+        result[r_idx] = .{.len = by_length[0].len, .flags = 1};
+        for (by_length[1..], 1..) |pat, p_idx| {
+            if (pat.len != result[r_idx].len) {
+                r_idx += 1;
+                result[r_idx] = .{.len = pat.len, .flags = 1 << p_idx};
+            } else
+                result[r_idx].flags |= 1 << p_idx;
+        }
+        break :init result;
+    });
+
+    comptime var init_table = [_]Flags{0} ** (std.math.maxInt(u8) + 1);
+    for (by_length, 0..) |pat, p_idx| for (pat) |c| {
+        init_table[c] |= 1 << p_idx;
+    };
+    const table = init_table;
+
+    comptime var init_enum_fields: [by_length.len]std.builtin.Type.EnumField = undefined;
+    for (&init_enum_fields, by_length, 0..) |*ef, p, i|
         ef.* = .{ .name = p, .value = i };
     const enum_fields = init_enum_fields;
 
-    const max_len, const min_len = init: {
-        var _longest = pats[0].len;
-        var _shortest = _longest;
-        for (pats[1..]) |p| {
-            _longest = @max(_longest, p.len);
-            _shortest = @min(_shortest, p.len);
-        }
-        break :init .{_longest, _shortest};
-    };
-
-    const Flags = @Type(.{ .int = .{ .bits = pats.len, .signedness = .unsigned } });
-
-    const CharInfo = struct {
-        valid_start: bool = false,
-        skip: u8 = pats[0].len,
-    };
-    const table_size = std.math.maxInt(u8) + 1;
-    comptime var init_char_info = [table_size]CharInfo{.{}};
-
-    comptime var init_offsets: [max_len]u8 = @splat(std.math.maxInt(u8));
-    comptime var max_span: usize = 0;
-    for (&init_offsets, 0..) |*offset, i| {
-        for (pats) |p| if (i < p.len) {
-            offset.* = @min(offset.*, p[i]);
-        };
-        for (pats) |p| max_span = @max(max_span, p[i] - offset.*);
-    }
-
-    const Masks = struct {
-        offset: u8,
-        masks: [max_span]Flags = .{0} ** max_span,
-    };
-    comptime var init_masks: Masks[max_len] = undefined;
-    for (&init_masks, init_offsets) |*mask, offset| {
-        mask.offset = offset;
-    }
-    for (pats, 0..) |p, p_idx| {
-        for ((&init_masks)[0..p.len], p) |*mask, c| {
-            const flag = 1 << p_idx;
-            mask.masks[c - mask.offset] |= flag;
-        }
-    }
-
-    const mask_table = init_masks;
-
     return struct {
         const Self = @This();
-
-        const Pattern = @Type(.{
+        pub const Pattern = @Type(.{
             .@"enum" = .{
                 .fields = &enum_fields,
                 .is_exhaustive = true,
@@ -76,65 +52,219 @@ fn Alternative(comptime patterns: []const [:0]const u8) type {
             },
         });
 
-        flags: Flags = std.math.boolMask(Flags, true),
-        col: usize = 0,
+        len_confirmed: Flags = 0,
+        char_confirmed: Flags = std.math.boolMask(Flags, true),
+        cur: usize = 0,
 
-        fn reset(self: *Self) void {
-            self.flags = std.math.boolMask(Flags, true);
-            self.col = 0;
+        fn check(self: *Self, text: []const u8) void {
+            inline for (lengths) |l| {
+                const stop: usize = @min(l.len, text.len);
+                const start: usize = @min(self.cur, stop);
+                for (text[start..stop]) |c| {
+                    self.char_confirmed &= table[c];
+                    self.cur += 1;
+                }
+                if (self.cur < stop) {
+                    @branchHint(.unlikely);
+                    return;
+                }
+                self.len_confirmed |= self.char_confirmed & l.flags;
+                self.char_confirmed &= ~l.flags;
+            }
         }
 
-        fn same(self: *Self, text: []const u8) usize {
-            const l = @min(text.len, max_len - self.col);
-            const s = self.col;
-            const pos = for (text[0..l], mask_table[s..l], 0..) |c, masks, i| {
-                const mask: Flags = masks.masks[c - masks.offset];
-                if (self.flags & mask == 0) break i;
-                self.flags &= mask;
-            } else l;
-            self.col += pos;
-            return pos;
+        pub const Result = union(enum) {
+            // No pattern matched
+            no_match,
+            // All characters in the tested text match the beginning of a pattern
+            possible_matches,
+            // The pattern that matches the beginning of the tested text
+            match: Pattern,
+        };
+
+        fn longest(self: *Self, text: []const u8) Result {
+            self.check(text);
+            // Look for long potentially unfinished matches first
+            while (self.char_confirmed != 0) {
+                const p_idx: u16 = @ctz(self.char_confirmed);
+                const p_string = by_length[p_idx][0..self.cur];
+                if (std.mem.eql(u8, p_string, text[0..p_string.len]))
+                    return .possible_matches;
+                self.char_confirmed &= switch (p_idx) {
+                    inline 0 ... by_length.len - 1 => |idx| ~(@as(Flags, 1) << idx),
+                    else => unreachable,
+                };
+            }
+
+            // If there were no potentially longer matches pending
+            while (self.len_confirmed != 0) {
+                const p_idx: u16 = @ctz(self.len_confirmed);
+                const p_string = by_length[p_idx];
+                if (std.mem.eql(u8, p_string, text[0..p_string.len]))
+                    return .{ .match = @enumFromInt(p_idx) };
+                self.len_confirmed &= switch (p_idx) {
+                    inline 0 ... by_length.len - 1 => |idx| ~(@as(Flags, 1) << idx),
+                    else => unreachable,
+                };
+            }
+
+            return .no_match;
         }
-
-
     };
 }
+
+const SkipTable = struct {
+    const size: usize = std.math.maxInt(u8) + 1;
+    min_len: u8,
+    table: [size]u8,
+
+    fn init(patterns: []const [:0]const u8) SkipTable {
+        var self: SkipTable = .{
+            .min_len = std.math.maxInt(u8),
+            .table = undefined,
+        };
+        for (patterns) |p| self.min_len = @min(self.min_len, p.len);
+        @memset(&self.table, self.min_len);
+        for (patterns) |p| for (p[0 .. self.min_len - 1], 0..) |c, pos| {
+            self.table[c] = @min(self.table[c], self.min_len - pos - 1);
+        };
+        return self;
+    }
+
+    fn skip(self: SkipTable, text: []const u8) usize {
+        return self.table[text[self.min_len - 1]];
+    }
+};
+
+// pub fn findInPos(haystack: []const u8, pos: usize) struct { usize, ?Pattern } {
+//     var _pos: usize = pos;
+//     var match_len: usize = 0;
+//     while (_pos < haystack.len - min_len and _pos + match_len != haystack.len) : ({
+//         _pos += skip_table[haystack[_pos + min_len - 1]];
+//     }) {
+//         if (!first_char[haystack[_pos]]) continue;
+//         match_len, const idx = same(haystack[_pos..]);
+//         if (patterns[idx].len == match_len)
+//             return .{ _pos, @enumFromInt(idx) };
+//     }
+//     while (_pos < haystack.len and match_len != haystack.len) : (_pos += 1) {
+//         if (!first_char[haystack[_pos]]) continue;
+//         match_len, _ = same(haystack[_pos..]);
+//     }
+//     while (_pos < haystack.len and !first_char[haystack[_pos]]) _pos += 1;
+//     return .{ _pos, null };
+// }
+
+const StreamCursor = struct {
+    reader: *std.Io.Reader,
+    writer: *std.Io.Writer,
+    data: []u8 = &.{},
+    read: usize = 0,
+    written: usize = 0,
+
+    fn stream(self: *@This(), n: usize) !void {
+        const bytes = self.data[0..n];
+        self.written += try self.writer.write(bytes);
+        self.read += n;
+        self.reader.toss(n);
+        self.data = self.data[n..];
+    }
+
+    fn replace(self: *@This(), n: usize, substitute: []const u8) !void {
+        self.written += try self.writer.write(substitute);
+        self.read += n;
+        self.reader.toss(n);
+        self.data = self.data[n..];
+    }
+
+    fn peekDelimiterExclusive(self: *@This(), delimiter: u8) !void {
+        self.data = self.reader.peekDelimiterExclusive(delimiter) catch |err| switch (err) {
+            error.StreamTooLong => {
+                self.data = self.reader.buffered();
+                return err;
+            },
+            else => return err,
+        };
+    }
+
+    fn streamReplaceDelimiter(self: *@This(), delimiter: u8) !usize {
+        const patterns: []const [:0]const u8 = &.{
+            "&amp;", "&gt;", "&lt;", "&quote;", "&apos;"
+        };
+        const skip_table = SkipTable.init(patterns);
+        var matcher = MatchCursor(patterns){};
+        var loop = true;
+        outer: while (loop) {
+            loop = false;
+            self.peekDelimiterExclusive(delimiter) catch |err| switch (err) {
+                // Make another loop when we have processed the buffer
+                error.StreamTooLong => loop = true,
+                else => return err,
+            };
+
+            inner: while (self.data.len > 0) : ({
+                const skip = @min(self.data.len, skip_table.skip(self.data));
+                try self.stream(skip);
+                matcher = .{};
+            }) {
+                const pat, const pat_string = switch (matcher.longest(self.data)) {
+                    .no_match => continue :inner,
+                    .possible_matches => {
+                        @branchHint(.unlikely);
+                        continue :outer;
+                    },
+                    .match => |pat| .{pat, @tagName(pat)},
+                };
+                try self.replace(pat_string.len, switch (pat) {
+                    .@"&amp;" => "&",
+                    .@"&gt;" => ">",
+                    .@"&lt;" => "<",
+                    .@"&quote;" => "\"",
+                    .@"&apos;" => "'",
+                });
+            }
+        }
+        if (self.data.len > 0)
+            try self.stream(self.data.len);
+        return self.read;
+    }
+};
 
 fn streamReplaceDelimiter(
     r: *std.Io.Reader,
     w: *std.Io.Writer,
     delimiter: u8,
 ) !usize {
-    return r.streamDelimiter(w, delimiter);
+    var cursor: StreamCursor = .{ .reader = r, .writer = w };
+    return cursor.streamReplaceDelimiter(delimiter);
 }
-
 
 test "streaming replacement" {
     const test_text =
-    \\<request name="sync">
-    \\  &lt;description summary=&quote;asynchronous roundtrip&quote;&gt;
-    \\    The sync request asks the server to emit the 'done' event...
-    \\
-    \\    The callback_data passed in the callback is undefined and should be
-    \\    ignored.
-    \\  &lt;/description&gt;
-    \\  &lt;arg name=&quote;callback&quote; type=&quote;new_id&quote; interface=&quote;wl_callback&quote;
-    \\       summary=&quote;callback object for the sync request&quote;/&gt;
-    \\</request>
+        \\<request name="sync">
+        \\  &lt;description summary=&quote;asynchronous roundtrip&quote;&gt;
+        \\    The sync request asks the server to emit the 'done' event...
+        \\
+        \\    The callback_data passed in the callback is undefined and should be
+        \\    ignored.
+        \\  &lt;/description&gt;
+        \\  &lt;arg name=&quote;callback&quote; type=&quote;new_id&quote; interface=&quote;wl_callback&quote;
+        \\       summary=&quote;callback object for the sync request&quote;/&gt;
+        \\</request>
     ;
     var fixed_reader = std.Io.Reader.fixed(test_text);
 
     const expected =
-    \\<request name="sync">
-    \\  <description summary="asynchronous roundtrip">
-    \\    The sync request asks the server to emit the 'done' event...
-    \\
-    \\    The callback_data passed in the callback is undefined and should be
-    \\    ignored.
-    \\  </description>
-    \\  <arg name="callback" type="new_id" interface="wl_callback"
-    \\       summary="callback object for the sync request"/>
-    \\</request>
+        \\<request name="sync">
+        \\  <description summary="asynchronous roundtrip">
+        \\    The sync request asks the server to emit the 'done' event...
+        \\
+        \\    The callback_data passed in the callback is undefined and should be
+        \\    ignored.
+        \\  </description>
+        \\  <arg name="callback" type="new_id" interface="wl_callback"
+        \\       summary="callback object for the sync request"/>
+        \\</request>
     ;
 
     const gpa = std.testing.allocator;
